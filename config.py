50c0099dfff4b0a9159b6aa37027a396f1cc7999
saurabhjoshi <saurabh.joshi190695@gmail.com>
2019-11-09 10:33:00 +0530

    Pagination Completed

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['saurabh.joshi190695@gmail.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
 
