e04cf6464f98a8ac14033369fec68503017b4728
saurabhjoshi <saurabh.joshi190695@gmail.com>
2019-11-11 23:44:33 +0530

    Implemented moment.js functionality to get last seen and timestamps better.

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,9 @@ import os
 import logging
 
 from flask import Flask
-from flask_mail import Mail
 from config import Config
+from flask_mail import Mail
+from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
@@ -16,6 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
